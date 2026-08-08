327c7a00eb3c40a1f677e9fc2d44423f1faf3db6
M-Haruki <105488840+M-Haruki@users.noreply.github.com>
2026-08-08 13:59:37 +0000

    web vite conf

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
-})
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+    proxy: {
+      "/api": {
+        target: "http://localhost:1323",
+        changeOrigin: true,
+      },
+    },
+  },
+});
